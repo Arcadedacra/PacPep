c34f636cd3d21ccb68d7d25c15327670af68f3ab
BuckarooBanzai88 <buckaroo@houseoften.com>
2023-07-21 19:19:09 -0700

    v1.0.0.2 - made with unity

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Arcadedacra-PacPep-1.0.0.2";
+const contentToCache = [
+    "Build/PacPep_UnityBuild.loader.js",
+    "Build/PacPep_UnityBuild.framework.js",
+    "Build/PacPep_UnityBuild.data",
+    "Build/PacPep_UnityBuild.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
